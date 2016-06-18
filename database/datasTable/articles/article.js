de4933ba1d3faa37fbd07281567d346b1b42964f
陈明康 <mingkang1993@qq.com>
2016-06-18 09:08:12 +0800

    sequelize update
    sequelize update

--- database/datasTable/articles/article.js
+++ database/datasTable/articles/article.js
@@ -38,7 +38,7 @@ article.hasMany(praise, {constraints: false, onDelete: 'CASCADE',onUpdate:'CASCA
 //sort_connect.belongsTo(labelSort, {constraints: false});  //sort_connect属于article
 //sort_connect.belongsTo(article, {constraints: false});  //sort_connect属于article
 
-settings.sync().complete(function(err){
+settings.sync().then(function(err){
 
 });
 //article.sync();//同步数据库
