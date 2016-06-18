7eec4446a206f3e9cb4a5b4b0b405712e76ed177
陈明康 <mingkang1993@qq.com>
2016-06-18 09:03:06 +0800

    sequelize update

--- database/settings.js
+++ database/settings.js
@@ -1,7 +1,8 @@
 var config=require('./config.json');
 var sequelize=require('sequelize');
 var db=new sequelize(config.database,config.user,config.password,config.option);
-db.authenticate().complete(function(err){//测试连接是否成功
+
+db.authenticate().then(function(err){//测试连接是否成功
     if(!!err){
         console.log('unable to connect to the database');
     }else{
